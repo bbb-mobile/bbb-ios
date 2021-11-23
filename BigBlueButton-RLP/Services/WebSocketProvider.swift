6d0c06013037adc61cd39ac8d500977f93aea38f
Milan Bojic <milan.bojic@zuehlke.com>
2021-11-23 12:28:32 +0100

    BBBIA-26 | Add WebRTC Service helper classes.

---
+++ BigBlueButton-RLP/Services/WebSocketProvider.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  WebRTC-Demo
+//
+//  Created by Milan Bojic on 23/11/2021.
+//
+
+import Foundation
+
+protocol WebSocketProvider: AnyObject {
+    var delegate: WebSocketProviderDelegate? { get set }
+    func connect()
+    func send(data: Data)
+}
+
+protocol WebSocketProviderDelegate: AnyObject {
+    func webSocketDidConnect(_ webSocket: WebSocketProvider)
+    func webSocketDidDisconnect(_ webSocket: WebSocketProvider)
+    func webSocket(_ webSocket: WebSocketProvider, didReceiveData data: Data)
+}
